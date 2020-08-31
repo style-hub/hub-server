61cc168e98426797d3694a2f55cc52f299b12bba
klakar <klas.karlsson@geosupportsystem.se>
2020-08-31 18:40:03 +0200

    Fix page numbering

--- source/styles.php
+++ source/styles.php
@@ -207,13 +207,13 @@
           <div class="row justify-content-md-center">
             <!-- navigation -->
             <?php 
-            if($startitem>$iteminterval){
+            if($startitem>=$iteminterval){
               echo('<a href="styles.php?firstitem='.($startitem-$iteminterval).'">
               <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
               </svg></a>');
             }
-            echo(' Page '.round(($startitem)/$iteminterval).' ');
+            echo(' Page '.ceil(($startitem)/$iteminterval+1).' ');
             if($itemscounter==$iteminterval){
               echo('<a href="styles.php?firstitem='.($startitem+$iteminterval).'">
               <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
